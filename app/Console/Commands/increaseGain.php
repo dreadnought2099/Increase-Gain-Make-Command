a93830a3fe065f7479c4222d80113a4ebd194f65
dreadnought2099 <magallanesraymart@gmail.com>
2025-01-17 15:55:03 +0800

    make command increase gain

---
+++ app/Console/Commands/increaseGain.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class increaseGain extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'increase:gain';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Increase investments gain.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
